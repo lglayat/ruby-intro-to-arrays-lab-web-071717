dd0337d01ba6722a7fa4f66ebe989a84f0ab80ae
Luke Glayat <glayatle@g.cofc.edu>
2017-07-07 04:01:15 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  arr = []
+  return arr
+end
+
+def array_with_two_elements
+  arr = ["a","b"]
+  return arr
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(arr)
+  return arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  return arr.shift
+end
+
+def last_element_with_array_methods(arr)
+  return arr.pop
+end
+
+def length_of_array(arr)
+  return arr.length
+end
